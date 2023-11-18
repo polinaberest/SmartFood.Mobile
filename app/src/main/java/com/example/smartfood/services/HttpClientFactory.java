c374bf89fdd767e7b49b780c7e795634056fdcb2
Polina Berest <polina.berest@nure.ua>
2023-11-18 14:00:41 +0200

    minor changes

--- app/src/main/java/com/example/smartfood/services/HttpClientFactory.java
+++ app/src/main/java/com/example/smartfood/services/HttpClientFactory.java
@@ -10,15 +10,15 @@ public class HttpClientFactory {
     private static HttpClientFactory instance;
     private OkHttpClient client;
 
-    private HttpClientFactory() {
+    private HttpClientFactory(Context context) {
         client = new OkHttpClient.Builder()
-                .addInterceptor(new AuthInterceptor(tokenService, authService))
+                .addInterceptor(new AuthInterceptor(new TokenStorageService(context), AuthService.getInstance(context)))
                 .build();
     }
 
-    public static HttpClientFactory getInstance() {
+    public static HttpClientFactory getInstance(Context context) {
         if (instance == null) {
-            instance = new HttpClientFactory();
+            instance = new HttpClientFactory(context);
         }
         return instance;
     }
